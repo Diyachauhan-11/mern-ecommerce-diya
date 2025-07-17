f39807ce6a5949ba39b5fead0032da8f902a2aff
Diya Chauhan <diyac490@gmail.com>
2025-07-17 14:21:06 -0400

    Initial commit

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -3,10 +3,12 @@ import "./aboutSection.css";
 import { Button, Typography, Avatar } from "@material-ui/core";
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import InstagramIcon from "@material-ui/icons/Instagram";
+
 const About = () => {
   const visitInstagram = () => {
-    window.location = "https://instagram.com/meabhisingh";
+    window.location = "https://instagram.com/diyac490";
   };
+
   return (
     <div className="aboutSection">
       <div></div>
@@ -18,28 +20,32 @@ const About = () => {
           <div>
             <Avatar
               style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-              src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/jpyibarlaxawvcvqjv5b.png"
+              src="https://avatars.githubusercontent.com/u/166870182?v=4"
               alt="Founder"
             />
-            <Typography>Abhishek Singh</Typography>
+            <Typography>Diya Chauhan</Typography>
             <Button onClick={visitInstagram} color="primary">
               Visit Instagram
             </Button>
             <span>
-              This is a sample wesbite made by @meabhisingh. Only with the
-              purpose to teach MERN Stack on the channel 6 Pack Programmer
+              This is a sample E-Commerce website developed by <strong>Diya Chauhan</strong> using the MERN Stack, for learning and portfolio purposes.
             </span>
           </div>
           <div className="aboutSectionContainer2">
-            <Typography component="h2">Our Brands</Typography>
+            <Typography component="h2">My Platforms</Typography>
             <a
-              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
-              target="blank"
+              href="https://www.youtube.com/@diyac490" // Optional, or replace with another platform
+              target="_blank"
+              rel="noreferrer"
             >
               <YouTubeIcon className="youtubeSvgIcon" />
             </a>
 
-            <a href="https://instagram.com/meabhisingh" target="blank">
+            <a
+              href="https://instagram.com/diyac490"
+              target="_blank"
+              rel="noreferrer"
+            >
               <InstagramIcon className="instagramSvgIcon" />
             </a>
           </div>
